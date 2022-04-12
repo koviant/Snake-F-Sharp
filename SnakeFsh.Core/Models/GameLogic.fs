b77ac547a9dca6e5e34bd06824b59287b5d2f2e4
Kiryl Siaukovich <k.siaukovich@gmail.com>
2022-04-13 00:07:57 +0200

    Added update snake position logic

--- SnakeFsh.Core/Models/GameLogic.fs
+++ SnakeFsh.Core/Models/GameLogic.fs
@@ -3,10 +3,12 @@ module Snake.Core.GameLogic
 open Snake.Core.Models
 
 let private getDefaultHead fieldSize = {
-            Head = { X = fieldSize / 2
-                     Y = fieldSize / 2 }
-            FieldSize = fieldSize
+        Head = {
+            X = fieldSize / 2
+            Y = fieldSize / 2
         }
+        FieldSize = fieldSize
+    }
 
 let private getDefaultState startData =
     let getStartSnake head = {
@@ -18,19 +20,12 @@ let private getDefaultState startData =
     let getDefaultField size = {
         Cells = [ for _ in 0..size ->
                     [ for _ in 0..size -> Cell.WithNone ] ]
-    }
+        }
 
     { Field = getDefaultField startData.FieldSize
       Snake = getStartSnake startData.Head }
 
-let private getNewHead head direction =
-    match direction with
-    | Up -> { head with X = head.X - 1 }
-    | Down -> { head with X = head.X + 1 }
-    | Left -> { head with Y = head.Y - 1 }
-    | Right -> { head with Y = head.Y + 1 }
-
-let private validate field newHead =
+let private getUpdate field newHead =
     match field.Cells[newHead.X][newHead.Y] with
     | Border -> Collided WithBorder
     | WithSnake _ -> Collided WithSelf
@@ -46,27 +41,33 @@ let private updateSnake snake changes =
                   | Grows -> snake.Head :: snake.Body
                   | StaysSameSize -> snake.Head :: (List.take (snake.Body.Length - 1) snake.Body)
 
-    { snake with Head = changes.NewHead
-                 Tail = newTail
-                 Body = newBody }
+    { Head = changes.NewHead
+      Tail = newTail
+      Body = newBody }
 
 let private updateField oldField updatedSnake =
-    let clearSnakeCells row =
-        let emptyIfSnake cell =
-            match cell with
-            | WithSnake _ -> WithNone
-            | _ -> cell
-        
-        row |> List.map emptyIfSnake
-    
-    // stub
-    let setSnakeCells cells =
-        [ WithNone ]
-    
-    oldField.Cells
-    |> List.map clearSnakeCells
-    |> List.map setSnakeCells
-    |> fun cells -> { Cells = cells }
+    let notNewTail i j =
+        i <> updatedSnake.Tail.X &&
+        j <> updatedSnake.Tail.Y
+
+    let mapSnakeCell i j cell snakePart =
+        match snakePart with
+        | Head dir when updatedSnake.Body.Length > 1 -> Cell.WithSnake <| Body dir
+        | Head _ -> cell
+        | Tail _ when notNewTail i j  -> Cell.WithNone
+        | Tail _ -> cell
+        | Body _ when notNewTail i j -> cell
+        | Body dir -> Cell.WithSnake <| Tail dir
+
+    {
+        Cells = [ for i in 0..oldField.Cells.Length ->
+                    [ for j in 0..oldField.Cells.Length ->
+                        match oldField.Cells[i][j] with
+                        | WithNone | WithFood | Border -> oldField.Cells[i][j]
+                        | WithSnake part -> mapSnakeCell i j oldField.Cells.[i].[j] part
+                    ]
+                ]
+    }
 
 let generateState oldState updatedSnake = {
         Snake = updatedSnake
@@ -82,8 +83,15 @@ let getStartState = getDefaultHead >> getDefaultState
 
 [<CompiledName "Update">]
 let update state direction =
+    let getNewHead head direction =
+        match direction with
+        | Up -> { head with X = head.X - 1 }
+        | Down -> { head with X = head.X + 1 }
+        | Left -> { head with Y = head.Y - 1 }
+        | Right -> { head with Y = head.Y + 1 }
+
     getNewHead state.Snake.Head direction
-    |> validate state.Field
+    |> getUpdate state.Field
     |> function
     | Collided collisionType -> GameOver collisionType
     | CanGo changes -> OngoingGame <| createState state changes
